a9f924d50db0473a222062217931f6214c796709
ben <kakengi_bernard@hotmail.com>
2022-02-15 07:33:12 +0200

    added frontend UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt, FaUser } from 'react-icons/fa';
 
 const Login = () => {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	const { email, password } = formData;
+
+	const handleChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<>
+			<section className="heading">
+				<h1>
+					<FaSignInAlt /> Login
+				</h1>
+				<p>Please Log In</p>
+			</section>
+
+			<section className="form">
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-control"
+							id="email"
+							name="email"
+							value={email}
+							placeholder="Enter your email"
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="form-group">
+						<input
+							type="text"
+							className="form-control"
+							id="password"
+							name="password"
+							value={password}
+							placeholder="Enter your password"
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className="form-group">
+						<button type="submit" className="btn btn-block">
+							Login
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
 	);
 };
 
